b35872174281b533666277df61a597ec545ec68b
Viraj Chokhany <viraj.chokhany@gmail.com>
2021-10-10 02:39:42 +0530

    update

--- DP/cut.java
+++ DP/cut.java
@@ -191,29 +191,37 @@ public class cut
     }
 
     //````````````````````````````````````````````````````````````````````````````````````````````````````````````````
-
+    
+    // min-max expression evaluation
     public static class pair
     {
         int min=(int)1e9;
         int max=-(int)1e9;
     }
+    
     public static pair minMax(String str)
     {
         int n=str.length();
-        return minMax(str,0,n-1);
+        pair dp[][]=new pair[n][n];
+        return minMax_tabu(str,0,n-1,dp);
     }
-    private static pair minMax(String str, int i, int j) 
+    
+    // memoisation
+    private static pair minMax(String str, int i, int j,pair dp[][]) 
     {
         if(i==j)
         {
             pair base=new pair();
             base.max=base.min=str.charAt(i)-'0';
+            return dp[i][j]=base;
         }
+        if(dp[i][j]!=null)
+            return dp[i][j];
         pair ans=new pair();
         for(int k=i+1;k<j;k=k+2)
         {
-            pair l=minMax(str, i, k-1);
-            pair r=minMax(str, k+1, j);
+            pair l=minMax(str, i, k-1, dp);
+            pair r=minMax(str, k+1, j, dp);
             if(str.charAt(k)=='+')
             {
                 // min
@@ -229,9 +237,51 @@ public class cut
                 ans.max=Math.max(ans.max, l.max*r.max);
             }
         }
-        return ans;
+        return dp[i][j]=ans;
     }
-
+    //tabulation
+    private static pair minMax_tabu(String str, int I, int J,pair dp[][]) 
+    {
+        int n=dp.length;
+        for(int gap=0;gap<n;gap++)
+        {
+            for(int i=0,j=gap;j<n;i++,j++)
+            {
+                if(i==j)
+                {
+                    pair base=new pair();
+                    base.max=base.min=str.charAt(i)-'0';
+                    dp[i][j]=base;
+                    continue;
+                }
+                
+                pair ans=new pair();
+                for(int k=i+1;k<j;k=k+2)
+                {
+                    pair l=dp[i][k-1];  //minMax(str, i, k-1, dp);
+                    pair r=dp[k+1][j];  //minMax(str, k+1, j, dp);
+                    if(str.charAt(k)=='+')
+                    {
+                        // min
+                        ans.min=Math.min(ans.min,l.min+r.min);
+                        //max 
+                        ans.max=Math.max(ans.max, l.max+r.max);
+                    }
+                    else
+                    {
+                        // min
+                        ans.min=Math.min(ans.min,l.min*r.min);
+                        //max 
+                        ans.max=Math.max(ans.max, l.max*r.max);
+                    }
+                }
+                dp[i][j]=ans;
+            }
+        }
+        return dp[I][J];
+    } 
+   
+    //```````````````````````````````````````````````````````````````````````````````````````````````````````````````
     public static void main(String[] args) 
     {
         // int p[]={40,20,30,10,30};
