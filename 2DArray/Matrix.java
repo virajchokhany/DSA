21980c63bb66a3d1a1b262024bf5382aa04d53b6
Viraj Chokhany <viraj.chokhany@gmail.com>
2022-02-05 04:56:46 +0530

    update

--- 2DArray/Matrix.java
+++ 2DArray/Matrix.java
@@ -64,6 +64,98 @@ public class Matrix
             }
         }
     }
+
+    public static void printSpiral(int arr[][])
+    {
+        int n=arr.length;
+        int m=arr[0].length;
+        int rmin=0,rmax=n-1,cmin=0,cmax=m-1;
+        int tne=n*m;
+
+        while(tne>0)
+        {
+            for(int r=rmin;r<=rmax && tne>0 ; r++)
+            {
+                System.out.print(arr[r][cmin]+" ");
+                tne--;
+            }
+            cmin++;
+            for(int c=cmin;c<=cmax && tne>0;c++)
+            {
+                System.out.print(arr[rmax][c]+" ");
+                tne--;
+            }
+            rmax--;
+            for(int r=rmax;r>=rmin && tne>0 ;r--)
+            {
+                System.out.print(arr[r][cmax]+" ");
+                tne--;
+            }
+            cmax--;
+            for(int c=cmax;c>=cmin && tne>0;c--)
+            {
+                System.out.print(arr[rmin][c]+" ");
+                tne--;
+            }
+            rmin++;
+        }
+    }
+
+    public static void exitPointOfMatrix(int arr[][])
+    {
+        int i=0,j=0,n=arr.length,m=arr[0].length;
+        int dir=0;
+        int r=0,c=0;
+        while(i>=0 && j>=0 && i<n && j<m)
+        {
+            r=i;
+            c=j;
+            if(arr[i][j]==0)
+            {
+                if(dir==0)
+                {
+                    j++;
+                }
+                else if(dir==1)
+                {
+                    i++;
+                }
+                else if(dir==2)
+                {
+                    j--;
+                }
+                else 
+                {
+                    i--;
+                }
+            }
+            else
+            {
+                if(dir==0)
+                {
+                    i++;
+                    dir=1;
+                }
+                else if(dir==1)
+                {
+                    j--;
+                    dir=2;
+                }
+                else if(dir==2)
+                {
+                    i--;
+                    dir=3;
+                }
+                else 
+                {
+                    j++;
+                    dir=0;
+                }
+            }
+        }
+        System.out.println(r);
+        System.out.println(c);
+    }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int arr[][]=new int[sc.nextInt()][sc.nextInt()];
@@ -84,6 +176,6 @@ public class Matrix
         // System.out.println();
         // displayVerticialChange(arr);
 
-        printUsingGapStrategy(arr);
+        exitPointOfMatrix(arr);
     }
 }
