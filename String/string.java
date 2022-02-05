3d6f302a5b5414ac483bf2a7b53f1b78a9899c23
Viraj Chokhany <viraj.chokhany@gmail.com>
2022-02-05 14:10:38 +0530

    update

--- String/string.java
+++ String/string.java
@@ -17,8 +17,27 @@ public class string
         return ans;
     }
 
+    public static String removeDuplicatesInsertCount(String s)
+    {
+        String ans="";
+        int i=0;
+        while(i<s.length())
+        {
+            int j=i;
+            while(j<s.length() && s.charAt(i)==s.charAt(j))
+            {
+                j++;
+            }
+            int count=j-i;
+            ans+=s.charAt(i);
+            if(count>1)
+                ans+=count;
+            i=j;
+        }
+        return ans;
+    }
     public static void main(String[] args) 
     {
-        System.out.println(removeDuplicates("aabbbccddeeeeef"));
+        System.out.println(removeDuplicatesInsertCount("aaabbbbbbccccddddefghiiijkl"));
     }
 }
